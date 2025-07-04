24da65e3b86a3fe4f54b4b13887cceca24e476f6
Anoop Vikram <anoopvikram619@gmail.com>
2025-07-05 01:10:18 +0530

    removed beverages section images

--- src/components/Beverages.jsx
+++ src/components/Beverages.jsx
@@ -6,31 +6,29 @@ import gsap from 'gsap'
 
 const Beverages = () => {
 
-    useGSAP(() =>{
-        const parallaxTimeline = gsap.timeline({
-            scrollTrigger: {
-                trigger:'#beverages',
-                start: 'top 30%',
-                end: 'bottom 80%',
-                scrub: true,
-            }
-        })
+    // useGSAP(() =>{
+    //     const parallaxTimeline = gsap.timeline({
+    //         scrollTrigger: {
+    //             trigger:'#beverages',
+    //             start: 'top 30%',
+    //             end: 'bottom 80%',
+    //             scrub: true,
+    //         }
+    //     })
 
-        parallaxTimeline
-        .from('#b-left-leaf', {
-            x:-100,
-            y:100
-        })
+    //     parallaxTimeline
+    //     .from('#b-left-leaf', {
+    //         x:-100,
+    //         y:100
+    //     })
 
-        .from('#b-right-leaf', {
-            x:100, 
-            y:100
-        })
-    })
+    //     .from('#b-right-leaf', {
+    //         x:100, 
+    //         y:100
+    //     })
+    // })
   return (
     <section id="beverages" className='noisy rounded-b-4xl'>
-        <img src="images/hero-left-leaf.png" alt="left-leaf" id="b-left-leaf"/>
-        <img src="images/hero-right-leaf.png" alt="right-leaf" id="b-right-leaf"/>
 
         <div className='list justify-evenly'>
             <div className='foods'>
